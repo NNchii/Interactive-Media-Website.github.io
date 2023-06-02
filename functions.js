9987b64dc6401791d52c36b067d0f91ce9ebe115
NNchii <lucienzhang200114@gmail.com>
2023-06-02 21:06:09 +0200

    Update for smooth scroll

--- functions.js
+++ functions.js
@@ -11,12 +11,73 @@ window.addEventListener("DOMContentLoaded", function() {
             // Get the target element
             var target = document.querySelector(this.getAttribute('href'));
 
-            // Scroll to the target element
-            target.scrollIntoView({
-                behavior: 'smooth'  // Smooth scrolling
-            });
+            // Scroll to the target element with a custom speed
+            scrollToElement(target, 1000);  // 1000 ms = 1 second
         });
     }
 });
+
+function scrollToElement(element, duration) {
+    var start = window.pageYOffset;
+    var target = element.getBoundingClientRect().top;
+    var startTime = null;
+
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, start, target, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+}
+
+var btn = document.getElementById("back-to-top-btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+btn.onclick = function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+};
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.opacity = "0";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  slides[slideIndex-1].style.opacity = "1";
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+
+
+
+
+
 
 
